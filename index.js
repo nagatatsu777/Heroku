eeb6ed74a684a62dd39c3c3496f69d4bd78e55d7
Lan Nagashima <Lan@Lans-MacBook-Pro.local>
2021-06-19 10:19:15 -0700

    update

--- index.js
+++ index.js
@@ -46,9 +46,6 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello Wor!</h1>')
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
