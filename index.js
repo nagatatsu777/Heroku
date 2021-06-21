0482f4934a32d99428c5f229bd9328eddda3b056
Lan Nagashima <Lan@Lans-MacBook-Pro.local>
2021-06-21 14:55:04 -0700

    update

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 //link: https://infinite-earth-35390.herokuapp.com
@@ -5,6 +6,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
+const Note = require('./model/note.js')
 let notes = [
   /*{
     id: 1,
@@ -47,36 +49,52 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
-app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+/*app.get('/api/notes/:id', (request, response) => {
+  /*const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
   if (note) {
     response.json(note)
   } else {
     response.status(404).end()
   }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 
+})*/
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
+
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
+  /*const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
-  response.status(204).end()
+  response.status(204).end()*/
+  Note.findByIdAndRemove(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
+
 app.post('/api/notes/:name/:number/:id', (request, response) => {
-  console.log(request.params.name)
   if (!request) {
     return response.status(400).json({ 
       error: 'content missing' 
     })
-  }
-  const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
   }
   /*const note = {
     content: body.content,
@@ -84,17 +102,47 @@ app.post('/api/notes/:name/:number/:id', (request, response) => {
     date: new Date(),
     id: generateId(),
   }*/
-  const note = {
+  const note = new Note({
     name: request.params.name,
     number: request.params.number,
     id: request.params.id
+  })
+  console.log(note.name+' '+note.number)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
+ // notes = notes.concat(note)
+
+  //response.json(note)
+})
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    name: body.name,
+    number: body.number,
+    id: body.id
   }
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
